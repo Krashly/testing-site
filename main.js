5cf46d28d92de41ba620ca5dfd9fff0cc62d36ea
Krashly <krashly@mail.ru>
2024-01-13 18:10:17 +0300

    Libs

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-hello
+const express = require('express');
+const app = express();
+
+const host = '127.0.0.1';
+const port = 7000;
+
+app.use(
+    '/uploads',
+    express.static(`${__dirname}/assets/img`)
+);
+app.use(
+    '/styles',
+    express.static(`${__dirname}/assets/css`)
+);
+
+app.listen(port, host, function () {
+    console.log(`Server listens http://${host}:${port}`);
+});
